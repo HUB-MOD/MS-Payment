0eb127d603c9b534e7a528d014676711956c474f
Heber Sixto Uraccahua Barrios <huraccahuab@gmail.com>
2022-03-22 01:40:40 -0500

    Add Dockerfile

--- MSPayment/src/main/java/com/nttdata/MSPayment/service/PaymentServiceBalancedImpl.java
+++ MSPayment/src/main/java/com/nttdata/MSPayment/service/PaymentServiceBalancedImpl.java
@@ -1,43 +1,42 @@
 package com.nttdata.MSPayment.service;
 
 import com.nttdata.MSPayment.model.Payment;
-import com.nttdata.MSPayment.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.domain.Sort;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Primary
 @Service
-public class PaymentServiceImpl implements PaymentService {
+public class PaymentServiceBalancedImpl implements PaymentService {
 
     @Autowired
-    PaymentRepository paymentRepository;
+    @Qualifier("wcLoadBalanced")
+    WebClient.Builder webClientBuilder;
 
     @Override
     public Mono<Payment> createPayment(Payment p) {
-        return paymentRepository.save(p);
+        return null;
     }
 
     @Override
     public Mono<Payment> updatePayment(Payment p) {
-        return paymentRepository.save(p);
+        return null;
     }
 
     @Override
     public Mono<Payment> findByPaymentId(Integer id) {
-        return paymentRepository.findById(id).switchIfEmpty(Mono.empty());
+        return null;
     }
 
     @Override
     public Flux<Payment> findAllByCreditId(Integer id) {
-        return paymentRepository.findAll(Sort.by(String.valueOf(id)));
+        return null;
     }
 
     @Override
     public Mono<Void> deletePayment(Integer id) {
-        return paymentRepository.deleteById(id);
+        return null;
     }
 }
